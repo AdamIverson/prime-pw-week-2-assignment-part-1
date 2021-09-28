c898901304f593177354cabe7c8a92645467eb74
Adam Iverson <aciverso@gmail.com>
2021-09-28 13:59:48 -0500

    completed required goals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -35,7 +35,7 @@ if (adventurous == true) {
 }
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-if ( luckyNumber == 2 && adventurous == true){
+if ( luckyNumber == 2 && adventurous == true) {
   console.log("Roll the dice!");
 }
 // 15 - Write a conditional that console logs "I can have more pets!"
@@ -43,7 +43,13 @@ if ( luckyNumber == 2 && adventurous == true){
 // console logs "I have enough pets" if the value of `pets` is equal to the value of `allowedPets`,
 // and console logs "Oh no, I have too many pets!"
 // if the value of `pets` is greater than the value of `allowedPets`.
-
+if (pets < allowedPets) {
+  console.log('I can have more pets!');
+} else if (pets == allowedPets) {
+  console.log('I have enough pets');
+} else if (pets > allowedPets) {
+  console.log('Oh no, I have too many pets!');
+}
 // STRETCH GOALS:
 
 // 16 - Make a variable called `mostPets` and a conditional that
